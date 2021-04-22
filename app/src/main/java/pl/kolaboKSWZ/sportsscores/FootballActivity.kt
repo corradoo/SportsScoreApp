d13cc4787cae667d012e934729be5b87ecc1dfc8
corradoo <konrado317@gmail.com>
2021-04-22 22:31:39 +0200

    Podłączony projekt z AS

---
+++ app/src/main/java/pl/kolaboKSWZ/sportsscores/FootballActivity.kt
@@ -0,0 +1,11 @@
+package pl.kolaboKSWZ.sportsscores
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FootballActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_football)
+    }
+}
