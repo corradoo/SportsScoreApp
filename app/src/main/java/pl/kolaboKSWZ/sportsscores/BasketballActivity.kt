85dbffecd82ac665d3e89c79726c7fd45d7132a1
sztrok <72755032+sztrok@users.noreply.github.com>
2021-06-04 15:28:30 +0200

    Pełna funkcjonalność koszykówki, api śmiga elegancko

--- app/src/main/java/pl/kolaboKSWZ/sportsscores/BasketballActivity.kt
+++ app/src/main/java/pl/kolaboKSWZ/sportsscores/BasketballActivity.kt
@@ -1,10 +1,12 @@
 package pl.kolaboKSWZ.sportsscores
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.DatePicker
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.createBitmap
@@ -18,14 +20,20 @@ import pl.kolaboKSWZ.sportsscores.BBMatchesAdapter
 import pl.kolaboKSWZ.sportsscores.R
 import pl.kolaboKSWZ.sportsscores.databinding.ActivityBasketballBinding
 import java.net.URL
+import java.util.*
+import kotlin.collections.ArrayList
 
-class BasketballActivity :AppCompatActivity(), BBMatchesAdapter.OnItemClickListener{
+class BasketballActivity :AppCompatActivity(), BBMatchesAdapter.OnItemClickListener, DatePickerDialog.OnDateSetListener{
 
     private lateinit var binding: ActivityBasketballBinding
     private var prevItem: MenuItem? =null
     private var dateItem: MenuItem? =null
     private lateinit var recyclerView: RecyclerView
     private val matchesList=ArrayList<BBMatch>()
+
+    var day = 0; var month = 0; var year = 0; var pickedDay = -1; var pickedMonth = -1
+    var pickedYear = -1; var formattedDay = ""; var formattedMonth = ""
+    lateinit var date : String
 
 
 
@@ -49,47 +57,47 @@ class BasketballActivity :AppCompatActivity(), BBMatchesAdapter.OnItemClickListe
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.basketball_menu,menu)
         prevItem=menu?.findItem(R.id.logo_bb)
-        dateItem=menu?.findItem(R.id.date_bb)
+
         dateItem?.setIcon(R.drawable.date)
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(item.itemId!= R.id.date_bb){
-            prevItem?.setShowAsAction(MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW)
-            item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
-            supportActionBar?.title=item.title
-            prevItem=item
-
-            when (item.itemId){
-                R.id.stand_west->{
-                    binding.toolbarBB.setBackgroundColor(resources.getColor(R.color.red))
-                    binding.toolbarBB.setTitleTextColor(resources.getColor(R.color.white))
-                    recyclerView.setBackgroundResource(R.color.west_bg)
-                    dateItem?.isVisible = false
-                }
-                R.id.stand_east->{
-                    binding.toolbarBB.setBackgroundColor(resources.getColor(R.color.blue))
-                    binding.toolbarBB.setTitleTextColor(resources.getColor(R.color.white))
-                    recyclerView.setBackgroundResource(R.color.east_bg)
-                    dateItem?.isVisible = false
-                }
-                R.id.logo_bb->{
-                    binding.toolbarBB.setBackgroundColor(resources.getColor(R.color.toolbar_bb))
-                    binding.toolbarBB.setTitleTextColor(resources.getColor(R.color.black))
-                    recyclerView.setBackgroundResource(R.color.grey)
-                    dateItem?.isVisible = true
-                    dateItem?.setIcon(R.drawable.date)
-                }
-
-            }
-
-        }
-        else{
-
-        }
-        return true
-    }
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+////        if(item.itemId!= R.id.date_bb){
+//            prevItem?.setShowAsAction(MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW)
+//            item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+//            supportActionBar?.title=item.title
+//            prevItem=item
+//
+//            when (item.itemId){
+//                R.id.stand_west->{
+//                    binding.toolbarBB.setBackgroundColor(resources.getColor(R.color.red))
+//                    binding.toolbarBB.setTitleTextColor(resources.getColor(R.color.white))
+//                    recyclerView.setBackgroundResource(R.color.west_bg)
+//                    dateItem?.isVisible = false
+//                }
+//                R.id.stand_east->{
+//                    binding.toolbarBB.setBackgroundColor(resources.getColor(R.color.blue))
+//                    binding.toolbarBB.setTitleTextColor(resources.getColor(R.color.white))
+//                    recyclerView.setBackgroundResource(R.color.east_bg)
+//                    dateItem?.isVisible = false
+//                }
+//                R.id.logo_bb->{
+//                    binding.toolbarBB.setBackgroundColor(resources.getColor(R.color.toolbar_bb))
+//                    binding.toolbarBB.setTitleTextColor(resources.getColor(R.color.black))
+//                    recyclerView.setBackgroundResource(R.color.grey)
+//                    dateItem?.isVisible = true
+//                    dateItem?.setIcon(R.drawable.date)
+//                }
+//
+//            }
+//
+////        }
+////        else{
+////
+////        }
+//        return true
+//    }
 
     override fun onItemClick(position: Int) {
 //        val intent= Intent(this,BBDetailActivity::class.java)
@@ -108,18 +116,22 @@ class BasketballActivity :AppCompatActivity(), BBMatchesAdapter.OnItemClickListe
 //        Toast.makeText(this, "position", Toast.LENGTH_SHORT).show()
     }
 
-    fun fabClick(view: View){
-        api()
-        /*matchesList.add(BBMatch(1,"LAL","110","GSW","98","LAL","2020-05-15"))
-        binding.recyclerBb.adapter?.notifyDataSetChanged()*/
-
-    }
     fun api() {
         val thread = Thread {
             try {
 
+
+                updateDateTime()
+                if(pickedDay==-1 || pickedMonth==-1 || pickedYear==-1){
+                    pickedDay=day
+                    pickedMonth=month
+                    pickedYear=year
+                }
+                matchesList.clear()
+                setDateValues()
+
                 val client = OkHttpClient()
-                val url = URL("https://www.balldontlie.io/api/v1/games?seasons[]=2020&start_date=2021-05-01")
+                val url = URL("https://www.balldontlie.io/api/v1/games?seasons[]=2020&start_date=$date")
 
                 val request = Request.Builder()
                     .url(url)
@@ -138,12 +150,13 @@ class BasketballActivity :AppCompatActivity(), BBMatchesAdapter.OnItemClickListe
 
                 val apiMatches = arrayListOf<BBMatch>()
 
+
                 objData.get("data").forEachIndexed { index, jsonNode ->
                     println("$index: ${jsonNode.get("homeName")} vs ${jsonNode.get("awayName")}")
                     val fullDate = jsonNode.get("date").toString().replace("\"", "").split("T")
                     apiMatches.add(BBMatch(
                         matchID = jsonNode.get("id").asInt(),
-                        matchLocation = jsonNode.get("home_team").get("city").toString().replace("\"", ""),
+                        matchLocation = jsonNode.get("home_team").get("abbreviation").toString().replace("\"", ""),
                         date = fullDate[0],
 
                         team1Name = jsonNode.get("home_team").get("full_name").toString().replace("\"", ""),
@@ -164,6 +177,47 @@ class BasketballActivity :AppCompatActivity(), BBMatchesAdapter.OnItemClickListe
         }
 
         thread.start()
+    }
+
+    fun BBDateClick(view: View){
+        updateDateTime()
+        DatePickerDialog(this,this,year,month,day).show()
+    }
+
+
+    private fun updateDateTime(){
+        val cal: Calendar = Calendar.getInstance()
+        day = cal.get(Calendar.DAY_OF_MONTH)
+        month = cal.get(Calendar.MONTH)
+        year = cal.get(Calendar.YEAR)
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        pickedDay = dayOfMonth
+        pickedMonth = month
+        pickedYear = year
+        setDateValues()
+    }
+
+    fun setDateValues() {
+        formattedDay = ""
+        formattedMonth = ""
+        if (pickedDay < 10) {
+            formattedDay += "0"
+        }
+        if (pickedMonth < 10) {
+            formattedMonth += "0"
+        }
+        formattedDay += pickedDay
+        formattedMonth += (pickedMonth + 1)
+        binding.BBDateButton.text = "$formattedDay.$formattedMonth.$pickedYear"
+        date = "$pickedYear-$formattedMonth-$formattedDay"
+    }
+
+
+
+    fun getApi(item: MenuItem) {
+        api()
     }
 
 }
